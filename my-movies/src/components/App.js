cc4d236e01b54068669abc71cfd0ae05cf5bb65b
Ali Ç <106672281+4liCicek@users.noreply.github.com>
2023-02-04 22:43:46 +0300

    Update App.js

--- my-movies/src/components/App.js
+++ my-movies/src/components/App.js
@@ -1,1 +1,53 @@
 
+import React from "react";
+import MovieList from "./MovieList";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  state = {
+    movies: [],
+
+    searchQuery: "",
+  };
+
+  async componentDidMount() {
+    const baseURL = "http://localhost:3002/movies";
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    this.setState({ movies: data });
+  }
+
+  deleteMovie = (movie) => {
+    const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+    this.setState((state) => ({
+      movies: newMovieList,
+    }));
+  };
+
+  searchMovie = (e) => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  render() {
+    let filteredMovies = this.state.movies.filter((movie) => {
+      return (
+        movie.name
+          .toLowerCase()
+          .indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      );
+    });
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12"></div>
+          <SearchBar searchMovieProp={this.searchMovie} />
+        </div>
+        <MovieList movies={filteredMovies} deleteMovieProp={this.deleteMovie} />
+      </div>
+    );
+  }
+}
+
+export default App;
